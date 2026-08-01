4bc404a7e75ef8c639c2bc78bf311e0a09f568b9
SudipBhakta98 <bhaktasudip1998@gmail.com>
2026-08-01 11:13:08 +0530

    update capabilities new image

--- image/image.js
+++ image/image.js
@@ -6,19 +6,6 @@ export const root_image = {
   twenty_years_of_excellence: "https://res.cloudinary.com/eaubwmsx/image/upload/q_auto,f_auto/v1784956676/ChatGPT_Image_Jul_25_2026_10_40_22_AM_mltwzo.png"
 }
 
-export const capabilities_image = {
-  febrication: "https://res.cloudinary.com/eaubwmsx/image/upload/q_auto,f_auto/v1784876893/WELDING_kpnh55.png",
-  powder_coating: "https://res.cloudinary.com/eaubwmsx/image/upload/q_auto,f_auto/v1784876892/POWDER_COATING_ubyeom.png",
-  assembly: "https://res.cloudinary.com/eaubwmsx/image/upload/q_auto,f_auto/v1784876891/ASSEMBLY_mcrpkx.png",
-  punching: "https://res.cloudinary.com/eaubwmsx/image/upload/q_auto,f_auto/v1784876890/TURRET_PUNCHING_tbtd71.png",
-  design_development: "https://res.cloudinary.com/eaubwmsx/image/upload/q_auto,f_auto/v1784876890/DESIGN_DEVELOPMENT_vnar6m.png",
-  tooling: "https://res.cloudinary.com/eaubwmsx/image/upload/q_auto,f_auto/v1784876889/IN_HOUSE_TOOLING_FACILITY_c4grqj.png",
-  laser_cuting: "https://res.cloudinary.com/eaubwmsx/image/upload/q_auto,f_auto/v1784876888/LASER_CUTTING_rsuuha.png",
-  stamping: "https://res.cloudinary.com/eaubwmsx/image/upload/q_auto,f_auto/v1784876887/STAMPING_pvczmy.png",
-  bending: "https://res.cloudinary.com/eaubwmsx/image/upload/q_auto,f_auto/v1784876887/BENDING_ekeunw.png"
-}
-
-
 
 
 // not use in the code keep object for future reference
